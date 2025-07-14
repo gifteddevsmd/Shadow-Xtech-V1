1670c450fa70191c5d8db83353e32256b9e2c0f9
Black-Tappy🏷️ <tappyblack1@gmail.com>
2025-07-14 22:38:48 +0300

    || Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SSSION_ID: process.env.SESSION_ID || "Shadow-Xtech~0dB2TQ4L#AtTE93df1R2htSyk8fkVTL5YRCga1GFWjuDTxCDPMjU",
+SESSION_ID: process.env.SESSION_ID || "Shadow-Xtech~0dB2TQ4L#AtTE93df1R2htSyk8fkVTL5YRCga1GFWjuDTxCDPMjU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
